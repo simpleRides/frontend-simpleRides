35c41c0b3f32d0b51247adc087b1892150ebfb85
Florencedesc <112195512+Florencedesc@users.noreply.github.com>
2022-10-24 15:37:03 +0200

    Frontend flo (#2)
    * first page
    
    * fix logo

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import HomeScreen from './screens/HomeScreen';
-import MapScreen from './screens/MapScreen';
 import PlacesScreen from './screens/PlacesScreen';
 
 import { Provider } from 'react-redux';
@@ -19,25 +18,23 @@ const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => {
   return (
-    <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ color, size }) => {
-          let iconName = '';
+    <Tab.Navigator screenOptions={({ route }) => ({
+      tabBarIcon: ({ color, size }) => {
+        let iconName = '';
 
-          if (route.name === 'Map') {
-            iconName = 'location-arrow';
-          } else if (route.name === 'Places') {
-            iconName = 'map-pin';
-          }
+        if (route.name === 'Map') {
+          iconName = 'location-arrow';
+        } else if (route.name === 'Places') {
+          iconName = 'map-pin';
+        }
 
-          return <FontAwesome name={iconName} size={size} color={color} />;
-        },
-        tabBarActiveTintColor: '#ec6e5b',
-        tabBarInactiveTintColor: '#335561',
-        headerShown: false,
-      })}
-    >
-      <Tab.Screen name="Map" component={MapScreen} />
+        return <FontAwesome name={iconName} size={size} color={color} />;
+      },
+      tabBarActiveTintColor: '#ec6e5b',
+      tabBarInactiveTintColor: '#335561',
+      headerShown: false,
+    })}>
+      <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="Places" component={PlacesScreen} />
     </Tab.Navigator>
   );
