1b79090f4130fc62cdd6ad3fda3dd7a389154767
kader Amrichat <kader.amrichat@gmail.com>
2022-11-01 21:46:51 +0100

    remove useless code after refacto

--- screens/RidesScreen.js
+++ screens/RidesScreen.js
@@ -158,7 +158,6 @@ export default function RidesScreen() {
   }
 
   // CARDS PROPS : timeToPickup, distanceTopickup, clientNote, markup, price, duration, pickupAddress, arrivalAddress, provider
-  // const [isChecked, setChecked] = useState(false);
   return (
     <ScrollView>
       <SafeAreaView style={styles.container}>
@@ -191,48 +190,6 @@ export default function RidesScreen() {
           isOpen={modalVisible}
           toggle={() => setModalVisible(!modalVisible)}
         />
-
-        {/* <Modal
-          animationType="slide"
-          transparent={false}
-          visible={modalVisible}
-          onRequestClose={() => setModalVisible(!modalVisible)}
-        >
-          <View style={styles.centeredView}>
-            <View style={styles.modalView}>
-              <View>
-                <SrText
-                  title="Paramétres de vos courses"
-                  subtitle="Filtrez vos courses selon vos critères préférés.Attention si la demande est trop faible nous vous en informerons et proposerons des courses hors critères"
-                />
-              </View>
-              <View style={styles.modalContent}>
-                <View style={styles.filterContainer}>
-                  <Checkbox
-                    style={styles.checkbox}
-                    value={isChecked}
-                    onValueChange={setChecked}
-                    color={isChecked ? colors.primary : undefined}
-                  />
-                  <View style={styles.sliderContainer}>
-                    <Text style={styles.sliderLabel}>Notation voyager</Text>
-                    <Slider
-                      style={{ width: 300, height: 40 }}
-                      minimumValue={0}
-                      maximumValue={10}
-                      minimumTrackTintColor="#FFFFFF"
-                      maximumTrackTintColor="#000000"
-                    />
-                  </View>
-                </View>
-              </View>
-              <SrButton
-                label="Valider"
-                handlePressed={() => setModalVisible(!modalVisible)}
-              />
-            </View>
-          </View>
-        </Modal> */}
       </SafeAreaView>
     </ScrollView>
   );
@@ -240,25 +197,6 @@ export default function RidesScreen() {
 
 const makeStyles = (colors) =>
   StyleSheet.create({
-    // filterContainer: {
-    //   flexDirection: 'row',
-    //   alignItems: 'center',
-    //   justifyContent: 'space-between',
-    //   width: '95%',
-    //   height: 80,
-    //   borderWidth: 1,
-    //   borderColor: colors.lightGrey,
-    //   paddingStart: 16,
-    //   borderRadius: 5,
-    //   backgroundColor: '#333',
-    // },
-    // sliderContainer: {
-    //   width: '100%',
-    //   paddingStart: 16,
-    // },
-    // sliderLabel: {
-    //   color: colors.light,
-    // },
     container: {
       flex: 1,
       backgroundColor: '#28272A',
@@ -302,27 +240,4 @@ const makeStyles = (colors) =>
       alignItems: 'center',
       justifyContent: 'space-between',
     },
-    // centeredView: {
-    //   flex: 1,
-    // },
-    // modalView: {
-    //   flex: 1,
-    //   backgroundColor: colors.black,
-    //   paddingHorizontal: 16,
-    //   paddingTop: Platform.OS === 'ios' ? 72 : 40,
-    //   paddingBottom: Platform.OS === 'ios' ? 32 : 24,
-    //   alignItems: 'center',
-    //   justifyContent: 'space-between',
-    // },
-    // textStyle: {
-    //   color: 'red',
-    //   fontWeight: 'bold',
-    //   textAlign: 'center',
-    // },
-    // modalText: {
-    //   color: colors.light,
-    //   fontSize: 20,
-    //   marginBottom: 15,
-    //   textAlign: 'center',
-    // },
   });
