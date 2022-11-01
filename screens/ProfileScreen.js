dc7ebe86b89f449de90f44c78799bd74d9bdffbf
Florencedesc <112195512+Florencedesc@users.noreply.github.com>
2022-11-01 15:44:52 +0100

    Profile page save 1 (#33)

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -2,11 +2,13 @@ import { View, Text, StyleSheet } from 'react-native';
 import React from 'react';
 import { useTheme } from '@react-navigation/native';
 import SrButton from '../components/core/SrButton';
+import SrText from '../components/core/SrText';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
-// const handleLogout = () => {
-//   dispatch(logout());
-//   dispatch(removeAllBookmark());
-// };
+//checker le padding du text, je n'arrive pas a centrer par rapport aux bordures du coup j'ai triché avec un padding, mais c'est pas clean
+//subcontainer le align items est KO et je ne sais pas pourquoi
+
+//créer constante logout
 
 const ProfileScreen = ({ navigation }) => {
   const { colors } = useTheme();
@@ -14,10 +16,35 @@ const ProfileScreen = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
-      <SrButton
-        label="Déconnexion"
-        handlePressed={() => navigation.navigate('Home')}
+      <SrText
+        title="Mon espace SimpleRides"
+        subtitle="Configurez votre espace et accédez à vos données"
       />
+      <View style={styles.textContainer}>
+        <Text style={styles.text}> Paramètres du compte </Text>
+        <Ionicons style={styles.chevron} iconName="chevron-forward-outline" />
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.text}>Intégrations avec mes apps</Text>
+      </View>
+      <Text style={styles.text}>Contacter le support</Text>
+      <View style={styles.textContainer}>
+        <Text style={styles.text}>Mode de paiement</Text>
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.text}>Supprimer mon compte</Text>
+      </View>
+      <View style={styles.subContainer}>
+        <SrButton
+          label="Retourner sur la page courses"
+          type="secondary"
+          handlePressed={() => navigation.navigate('Ride')}
+        />
+        <SrButton
+          label="Me déconnecter"
+          handlePressed={() => navigation.navigate('Home')}
+        />
+      </View>
     </View>
   );
 };
@@ -31,5 +58,33 @@ const makeStyles = (colors) =>
       justifyContent: 'center',
       alignItems: 'center',
       paddingHorizontal: 16,
+    },
+    chevron: {
+      width: 10,
+      height: 10,
+      color: colors.primary,
+    },
+    text: {
+      width: '100%',
+      height: 48,
+      marginTop: 10,
+      backgroundColor: colors.black,
+      borderColor: colors.lightGrey,
+      borderStyle: 'solid',
+      borderWidth: 1,
+      color: colors.light,
+      fontSize: 16,
+      borderRadius: 10,
+      marginBottom: 10,
+      paddingLeft: 8,
+      paddingTop: 12,
+    },
+    textContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+    },
+    subContainer: {
+      width: '100%',
+      alignItems: 'end',
     },
   });
