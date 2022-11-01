ba165cea60d504d7190f555e0279ef6877dc40bf
kader Amrichat <kader.amrichat@gmail.com>
2022-11-01 12:14:43 +0100

    factorize toasts to a global file

--- core/constants.js
+++ core/constants.js
@@ -1,6 +1,6 @@
 const constants = {
   // Renseigner l'adresse Ip de l'ordinateur
-  BACKEND_URL: 'http://192.168.10.184:3000',
+  BACKEND_URL: 'http://192.168.1.69:3000',
 };
 
 export default constants;
