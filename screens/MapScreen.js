c1f7f5f39ce2cd5020c321524856d06374d8c973
Gaël Moigne <gmoigne@live.fr>
2022-11-03 16:15:14 +0100

    mapScreen ok super cool

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -5,6 +5,7 @@ import MapViewDirections from 'react-native-maps-directions';
 import { Marker } from 'react-native-maps';
 import { useSelector } from 'react-redux';
 import { useTheme } from '@react-navigation/native';
+import { useRef, useState, useEffect } from 'react';
 
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
@@ -16,43 +17,47 @@ export default function MapScreen({ navigation }) {
   const map = useSelector((state) => state.map.value);
   const handleSubmit = () => navigation.navigate('Rides');
   const { colors } = useTheme();
+  const mapRef = useRef(null);
+  const [origin, setOrigin] = useState(null);
+  const [destination, setDestination] = useState(null);
+
+  useEffect(() => {
+    setOrigin({
+      latitude: map.pickupCoordinates.lat,
+      longitude: map.pickupCoordinates.lon,
+    });
+    setDestination({
+      latitude: map.arrivalCoordinates.lat,
+      longitude: map.arrivalCoordinates.lon,
+    });
+  }, []);
+
+  const edgePaddingValue = 70;
+
+  const edgePadding = {
+    top: edgePaddingValue,
+    right: edgePaddingValue,
+    bottom: edgePaddingValue,
+    left: edgePaddingValue,
+  };
+
+  const moveTo = () =>
+    mapRef.current.fitToCoordinates([origin, destination], {
+      edgePadding,
+      animated: false,
+    });
 
   return (
     <>
       <MapView
-        minZoomLevel={13}
+        ref={mapRef}
+        minZoomLevel={10}
+        onLayout={() => moveTo()}
         mapType="standard"
-        showsBuildings={false}
-        initialRegion={{
-          latitude: 48.866669,
-          longitude: 2.33333,
-          latitudeDelta: 0,
-          longitudeDelta: 0,
-        }}
-        animateToRegion={{
-          latitude: map.pickupCoordinates.lat,
-          longitude: map.pickupCoordinates.lon,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-        region={
-          map.pickupCoordinates && {
-            latitude: map.pickupCoordinates.lat,
-            longitude: map.pickupCoordinates.lon,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }
-        }
         style={{ flex: 1 }}
       >
-        {map.pickupCoordinates && (
-          <Marker
-            title="Pickup"
-            coordinate={{
-              latitude: map.pickupCoordinates.lat,
-              longitude: map.pickupCoordinates.lon,
-            }}
-          >
+        {origin && (
+          <Marker title="Pickup" coordinate={origin}>
             <Text
               style={{
                 backgroundColor: colors.primary,
@@ -65,14 +70,8 @@ export default function MapScreen({ navigation }) {
             <Ionicons name="pin" size={40} style={{ paddingRight: 5 }} />
           </Marker>
         )}
-        {map.arrivalCoordinates && (
-          <Marker
-            title="Arrival"
-            coordinate={{
-              latitude: map.arrivalCoordinates.lat,
-              longitude: map.arrivalCoordinates.lon,
-            }}
-          >
+        {destination && (
+          <Marker title="Arrival" coordinate={destination}>
             <Text
               style={{
                 backgroundColor: colors.primary,
@@ -87,15 +86,9 @@ export default function MapScreen({ navigation }) {
         )}
         {map && (
           <MapViewDirections
-            origin={{
-              latitude: map.pickupCoordinates.lat,
-              longitude: map.pickupCoordinates.lon,
-            }}
-            destination={{
-              latitude: map.arrivalCoordinates.lat,
-              longitude: map.arrivalCoordinates.lon,
-            }}
-            apikey="AIzaSyA-bKS86fQ7PK9wJ4gIG07PSA8k2meA4B4"
+            origin={origin}
+            destination={destination}
+            apikey={GOOGLE_API_KEY}
             strokeWidth={6}
             strokeColor={colors.primary}
           />
